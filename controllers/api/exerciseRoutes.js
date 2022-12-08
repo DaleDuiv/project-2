012390ac02dd8618cab82f05d01612404767726c
brodie02 <marshallbrodie0@gmail.com>
2022-12-08 19:35:03 +1030

    Add exercise post route and fix user post

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -2,5 +2,16 @@ const router = require('express').Router();
 const { Exercise } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newExercise = await Exercise.create({
+        exercise: req.session.exercise,
+      });
+  
+      res.status(200).json(newExercise);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
 
 module.exports = router;
