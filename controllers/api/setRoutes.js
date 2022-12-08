1e8cb619db4c9e490ba1a1156a1156d90d91ca07
brodie02 <marshallbrodie0@gmail.com>
2022-12-08 19:42:16 +1030

    Add post route

--- controllers/api/setRoutes.js
+++ controllers/api/setRoutes.js
@@ -2,5 +2,17 @@ const router = require('express').Router();
 const { Set } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newSet = await Set.create({
+        reps: req.session.reps,
+        weight: req.session.weight,
+      });
+  
+      res.status(200).json(newExercise);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
 
 module.exports = router;
