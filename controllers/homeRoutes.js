2b1f2f46c4925fa61b0bb6d432528fa80e9948a3
brodie02 <marshallbrodie0@gmail.com>
2022-12-08 19:54:26 +1030

    Add login get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,13 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+});
+
+module.exports = router;
