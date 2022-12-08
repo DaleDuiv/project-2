29a3a103a31258bef0fb91ba3ad9ecbdaf2b65a7
brodie02 <marshallbrodie0@gmail.com>
2022-12-08 19:38:28 +1030

    Add delete route

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -6,6 +6,7 @@ router.post('/', withAuth, async (req, res) => {
     try {
       const newExercise = await Exercise.create({
         exercise: req.session.exercise,
+        user_id: req.session.user_id,
       });
   
       res.status(200).json(newExercise);
@@ -13,5 +14,25 @@ router.post('/', withAuth, async (req, res) => {
       res.status(400).json(err);
     }
 });
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const exerciseData = await Exercise.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!exerciseData) {
+        res.status(404).json({ message: 'No exercise found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(exerciseData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
