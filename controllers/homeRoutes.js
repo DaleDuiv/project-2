09feab4d1cd40530b2d2e699c291371d9409bd1b
brodie02 <marshallbrodie0@gmail.com>
2022-12-13 19:25:33 +1030

    Add get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
-const { User, Exercise } = require('../models')
+const { User, Exercise, Set } = require('../models')
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
@@ -24,6 +24,34 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
         exercises,
+        logged_in: req.session.logged_in
+    })
+} catch (error) {
+    res.status(500).json(error)
+}
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const exerciseData = await Exercise.findAll({
+        where: {
+          user_id: req.session.user_id
+        }
+    });
+
+    const setData = await Set.findAll({
+      where: {
+        exercise_id: req.params.id
+      }
+    });
+
+    const exercises = exerciseData.map((exercise) => exercise.get({ plain: true }));
+
+    const sets = setData.map((set) => set.get({ plain: true }));
+
+    res.render('sets', {
+        exercises,
+        sets,
         logged_in: req.session.logged_in
     })
 } catch (error) {
