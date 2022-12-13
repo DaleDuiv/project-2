7e7b73750efcbfbeefe8b2e13aa537b69a8d3e92
brodie02 <marshallbrodie0@gmail.com>
2022-12-13 19:21:51 +1030

    Update get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { User, Exercise } = require('../models')
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
@@ -11,10 +12,23 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/', (req, res) => {
-  res.render('homepage', {
-    logged_in: req.session.logged_in
-  });
+router.get('/', async (req, res) => {
+  try {
+    const exerciseData = await Exercise.findAll({
+        where: {
+          user_id: req.session.user_id
+        }
+    });
+
+    const exercises = exerciseData.map((exercise) => exercise.get({ plain: true }));
+
+    res.render('homepage', {
+        exercises,
+        logged_in: req.session.logged_in
+    })
+} catch (error) {
+    res.status(500).json(error)
+}
 });
 
 module.exports = router;
